f6e6778ad4ec426047e8e6fdc8b1adc161422f03
tomm1341 <tommaso.foca@studio.unibo.it>
2023-11-09 17:13:17 +0100

    Implemented setUp and testWithdraw

--- 01-junit-bankaccounts/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
+++ 01-junit-bankaccounts/src/test/java/it/unibo/bank/impl/TestStrictBankAccount.java
@@ -1,5 +1,34 @@
 package it.unibo.bank.impl;
 
+import static org.junit.jupiter.api.Assumptions.abort;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.bank.api.AccountHolder;
+import it.unibo.bank.api.BankAccount;
+
 public class TestStrictBankAccount {
-    
+    private AccountHolder aRossi;
+    private AccountHolder aBianchi;
+
+    @BeforeEach
+    public void setUp() {
+        this.aRossi = new AccountHolder("Andrea", "Rossi", 1);
+        this.aBianchi = new AccountHolder("Alex", "Bianchi", 2);
+    }
+
+    @Test
+    public void testWithdwaw() {
+        final double initialAmount = 10000;
+        BankAccount account = new StrictBankAccount(aBianchi, initialAmount);
+        account.withdraw(aBianchi.getUserID(), 5000);
+        Assertions.assertEquals(account.getBalance(), 5000);
+        account.withdraw(aBianchi.getUserID(), 10000);
+        Assertions.assertEquals(account.getBalance(), 5000);
+        Assertions.assertEquals(account.getTransactionsCount(), 1);
+
+    }
+
 }
